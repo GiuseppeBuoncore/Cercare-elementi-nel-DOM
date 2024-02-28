7d7b726cfd59bc0f032a07fb46de264e578f1e1a
GiuseppeBuoncore <135731752+GiuseppeBuoncore@users.noreply.github.com>
2024-02-28 11:22:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const pClass = document.querySelectorAll('.paragrafo')
+console.log(pClass)
+
+const id = document.querySelector('#elemento-3')
+console.log(id)
+
+const allH2 = document.querySelectorAll('h2')
+console.log(allH2)
